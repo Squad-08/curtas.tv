d755c8f0512d0cc4f6a4df396b68374f7c3b1ef3
Geverson <geversonjosedesouza@gmail.com>
2021-04-29 21:59:15 -0300

    Exemplo págian inicial

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 const PaginaInicial = () => {
     return (
         <>
-        <h1>Página inicial</h1>
-        <Link to='/filme'>Ir para filme</Link>
+            <div className="card" >
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <Link className="btn btn-primary" to='/filme'>Ir para filme</Link>
+                </div>
+            </div>
         </>
     );
 }
