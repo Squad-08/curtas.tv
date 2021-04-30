77392684b62c8df9b82fd98fce9f68b8b14d707e
Matheus Leonardo <matheusleotrab@gmail.com>
2021-04-30 18:08:32 -0300

    Modifica disposiçao do Carrousel Genre
    Modifica qual e o comportamento do Carroussel Genre dentro da pagina e como os items dentro dele (poster e titulo) se comportam dentro dele. DETALHE: ainda esta estatico, falata o javascript dele

--- src/components/CarrouselGenre/index.jsx
+++ src/components/CarrouselGenre/index.jsx
@@ -9,19 +9,38 @@ const CarrouselGenre = () => {
         <section class="genres"> 
 
             <section className="short-genre">
-
                 <h2 className="genre-title">Gênero dos Curtas</h2>
-                <button class="button-arrow-left"><span class="direction">Anterior</span></button>
-                
-                <figure className="wrapper-poster">
-                    <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
-                    <figcaption className="short-title">Título do Curta</figcaption>
-                </figure>
-                
-                <button class="button-arrow-right"><span class="direction">Próximo</span></button>
-            
-            </section>
+                <div className="carroussel-genre">
+                    <button class="button-arrow-left"><span class="direction">Anterior</span></button>
+                    
+                    <figure className="wrapper-poster">
+                        <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
+                        <figcaption className="short-title">Sweet Tooth</figcaption>
+                    </figure>
 
+                    <figure className="wrapper-poster">
+                        <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
+                        <figcaption className="short-title">Sweet Tooth</figcaption>
+                    </figure>
+
+                    <figure className="wrapper-poster">
+                        <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
+                        <figcaption className="short-title">Sweet Tooth</figcaption>
+                    </figure>
+
+                    <figure className="wrapper-poster">
+                        <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
+                        <figcaption className="short-title">Sweet Tooth</figcaption>
+                    </figure>
+
+                    <figure className="wrapper-poster">
+                        <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
+                        <figcaption className="short-title">Sweet Tooth</figcaption>
+                    </figure>
+                    
+                    <button class="button-arrow-right"><span class="direction">Próximo</span></button>
+                </div>
+            </section>
         </section>
 
     );
