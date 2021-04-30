c357e21d6ae7f818715e19666e5fe35d3523231a
Matheus Leonardo <matheusleotrab@gmail.com>
2021-04-30 14:31:52 -0300

    Modificar CarrouselGenre
    Modifica o caminho de onde o arquivo index.jsx esta importando o arquivo base.css

--- src/components/CarrouselGenre/index.jsx
+++ src/components/CarrouselGenre/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
-import '../../assets/styles/base.css';
+import '../../assets/styles/base.css'
 
 const CarrouselGenre = () => {
     return (
