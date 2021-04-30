c169fabd790b4df9dab6a5ce8f04582251fec072
Matheus Leonardo <matheusleotrab@gmail.com>
2021-04-30 17:33:14 -0300

    Modifica CarrouselGenre
    Modifica classe do index.jsx do CarrouselGenre

--- src/components/CarrouselGenre/index.jsx
+++ src/components/CarrouselGenre/index.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.css';
-import '../../assets/styles/base.css'
+import './button-arrow.css';
+import '../../assets/styles/base.css';
 
 const CarrouselGenre = () => {
     return (
-        <section class="genres">
+        <section class="genres"> 
+
             <section className="short-genre">
+
                 <h2 className="genre-title">Gênero dos Curtas</h2>
                 <button class="button-arrow-left"><span class="direction">Anterior</span></button>
-                <figure className="wrapper-banner">
-                    <img src="" alt="" className="short-poster"/>
+                
+                <figure className="wrapper-poster">
+                    <img src="https://m.media-amazon.com/images/M/MV5BZDMxNjhiZmYtY2YyMC00NWFkLWI0ZWEtYmJkZThhMjlhYWE1XkEyXkFqcGdeQXVyMjUxMTY3ODM@._V1_UY268_CR43,0,182,268_AL_.jpg" alt="Poster Sweet Tooth" className="short-poster"/>
                     <figcaption className="short-title">Título do Curta</figcaption>
                 </figure>
+                
                 <button class="button-arrow-right"><span class="direction">Próximo</span></button>
+            
             </section>
+
         </section>
 
     );
