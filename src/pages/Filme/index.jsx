2e5144a6a2b5b13d44e2358ac64234570b5eb8f5
GisellePiasetzki <piasetzki.giselle@gmail.com>
2021-04-29 23:14:48 -0300

    adicionando os componentes SinopseTelaFilme e Video

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import BannerTelaFilme from '../../components/BannerTelaFilme'
+import SinopseTelaFilme from '../../components/SinopseTelaFilme'
+import Video from '../../components/Video'
 
 const Filme = () => {
     return (
         <>
             <h1>Filme selecionado</h1>
-            <BannerTelaFilme />
+            <BannerTelaFilme /> 
+            <SinopseTelaFilme />
+            <Video />
         </>
     );
 }
