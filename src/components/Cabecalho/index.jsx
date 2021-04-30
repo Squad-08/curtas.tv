2fc2e1e486064651a42ea0fb64259613eda97d12
Matheus Leonardo <matheusleotrab@gmail.com>
2021-04-30 13:46:49 -0300

    Modificar Cabecalho
    Modifica o arquivo index.js do cabecalho para se tornar index.jsx pra melhor compatibilidade, alem de modificar o codigo interno dele para adicionar o Componente corretamente

---
+++ src/components/Cabecalho/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Cabecalho = () => {
+    return (
+        <header className="main-header">
+            <Link className="site-logo" to='/'>Curtas<span className="dot">.</span>Tv</Link>
+
+            <Link className="home" to='/'>home</Link>
+        </header>
+    );
+}
+
+export default Cabecalho;
