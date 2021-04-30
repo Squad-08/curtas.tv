41aa668168334bf3d9f878a9b77b1c00570fd58a
Amicuchi <andersonamicuchi@gmail.com>
2021-04-29 21:58:37 -0300

    importação do mainbanner

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { mainBanner } from '../../components/mainBanner'
+import mainBanner from '../../components/mainBanner';
 
 const PaginaInicial = () => {
     return (
         <>
+            <mainBanner /> 
 
             <h1>Página inicial</h1>
             <br />
