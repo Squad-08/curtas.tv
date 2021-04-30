469f8472b9c9fe410eda775fc6187382e07ce841
Matheus Leonardo <matheusleotrab@gmail.com>
2021-04-30 14:20:08 -0300

    Adicionar arquivos CarrouselGenre
    Criar pasta com os arquivos index.jsx e styles.css referente ao carrossel com os filmes agrupados por generos. Sujeito a componetizaçao do das partes menores do Carrosel como poster e titulo para deixar essa parte responsavel apenas pelo carrossel

---
+++ src/components/CarrouselGenre/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+import '../../assets/styles/base.css';
+
+const CarrouselGenre = () => {
+    return (
+        <section class="genres">
+            <section className="short-genre">
+                <h2 className="genre-title">Gênero dos Curtas</h2>
+                <button class="button-arrow-left"><span class="direction">Anterior</span></button>
+                <figure className="wrapper-banner">
+                    <img src="" alt="" className="short-poster"/>
+                    <figcaption className="short-title">Título do Curta</figcaption>
+                </figure>
+                <button class="button-arrow-right"><span class="direction">Próximo</span></button>
+            </section>
+        </section>
+
+    );
+}
+
+export default CarrouselGenre
