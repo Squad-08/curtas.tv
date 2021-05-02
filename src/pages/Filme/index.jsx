1c88ce519e5c4d15daae8cf4c3a31f3e3391152d
Geverson <geversonjosedesouza@gmail.com>
2021-05-02 13:05:14 -0300

    Integração da tele do filme selecionado com a API

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,21 +1,37 @@
-import React from 'react';
-import BannerTelaFilme from '../../components/BannerTelaFilme'
-import SinopseTelaFilme from '../../components/SinopseTelaFilme'
-import TituloTelaFilme from '../../components/TituloTelaFilme'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as asctionsFilme from '../../core/actions/actionsFilme';
+import BannerTelaFilme from '../../components/BannerTelaFilme';
+import SinopseTelaFilme from '../../components/SinopseTelaFilme';
+import TituloTelaFilme from '../../components/TituloTelaFilme';
 import '../../assets/styles/base.css';
 import Embed from '../../components/Embed';
 
-const Filme = () => {
-    return (
-        <>
-        <div>
-            <TituloTelaFilme />
-            <BannerTelaFilme /> 
-            <SinopseTelaFilme />
-            <Embed />            
-        </div>
-        </>
-    );
+class Filme extends Component {
+
+    buscarFilme(id) {
+        //this.props.buscarFilme(id);
+    }
+
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.buscarFilme(id);
+    }
+
+    render() {
+        return (
+            <>
+                <TituloTelaFilme />
+                <BannerTelaFilme />
+                <SinopseTelaFilme />
+                <Embed />
+            </>
+        );
+    }
 }
 
-export default Filme;
+const mapStateToProps = state => ({
+    filme: state.filme.filme
+});
+
+export default connect(mapStateToProps, asctionsFilme)(Filme);
