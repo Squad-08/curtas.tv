3e02e895016fcf6404836c4e317ce4c27504576c
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-02 20:41:42 -0300

    Adicionar elementos ao MainCarroussel
    Adiciona novos elementos ao MainCarroussel como os botoes de setas laterais, uma thumbnail de exemplo e um titulo de exemplo

--- src/components/MainCarroussel/index.jsx
+++ src/components/MainCarroussel/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './styles.css';
+import shortThumbnail1 from './img/shortThumbnail1.png'
+import ButtonArrowLeft from '../ButtonArrowLeft';
+import ButtonArrowRight from '../ButtonArrowRight';
 
 const MainCarroussel = () => {
-    return ''
+    return (
+
+        <section className='main-carroussel'>
+            <ButtonArrowLeft />
+            <img src={shortThumbnail1} alt="Thumbnail do Curta"/>
+            <h2 className='movie-title-carroussel'>Here Is The Plan</h2>
+            <ButtonArrowRight />
+        </section>
+    )
 }
 
 export default MainCarroussel
