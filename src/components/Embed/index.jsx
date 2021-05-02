6976eccd345a7ef5856d1cfa61bdce82f89a4264
Geverson <geversonjosedesouza@gmail.com>
2021-05-02 13:54:53 -0300

    Definição de layout do Embed

--- src/components/Embed/index.jsx
+++ src/components/Embed/index.jsx
@@ -2,23 +2,17 @@ import React from 'react';
 import './styles.css';
 
 
-const YoutubeEmbed = (embedId) => {
+const Embed = (embedId) => {
     return (
         <>
-            <div class="embed-responsive-embed-responsive-16by9">
-            <iframe 
-            width="853"
-            height="480"
-            src={`https://www.youtube.com/embed/zpOULjyy-n8?rel=0${embedId}`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube" 
-            />
+            <div className="embed">
+                <iframe className="embed-iframe"
+                    src={`https://www.youtube.com/embed/zpOULjyy-n8?rel=0${embedId}`}
+                    title="Embedded youtube"
+                />
             </div>
         </>
     );
 }
 
-
-export default YoutubeEmbed;
+export default Embed;
--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -4,6 +4,7 @@ import * as asctionsFilme from '../../core/actions/actionsFilme';
 import BannerTelaFilme from '../../components/BannerTelaFilme';
 import SinopseTelaFilme from '../../components/SinopseTelaFilme';
 import TituloTelaFilme from '../../components/TituloTelaFilme';
+//import ScriptEmbedJS from '../../assets/js/embed';
 import '../../assets/styles/base.css';
 import Embed from '../../components/Embed';
 
@@ -11,6 +12,12 @@ class Filme extends Component {
 
     buscarFilme(id) {
         //this.props.buscarFilme(id);
+
+        //Adicionando a tag <script> para a importação do vídeo
+        // const script = document.createElement("script");
+        // script.src = { ScriptEmbedJS };
+        // script.async = true;
+        // document.body.appendChild(script);
     }
 
     componentDidMount() {
