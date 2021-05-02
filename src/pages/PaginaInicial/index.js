e7e153d862849fe50e933b91f0d8b18dd3d28836
Geverson <geversonjosedesouza@gmail.com>
2021-05-02 12:42:10 -0300

    Integração da página inicial com API

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import MainBanner from "../../components/MainBanner";
 import CarrouselGenre from "../../components/CarrouselGenre";
-// import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import * as actionsFilmes from '../../core/actions/actionsFilme';
 
-const PaginaInicial = () => {
-  return (
-    <>
-      <MainBanner />
-      <CarrouselGenre />
-    </>
-  );
+class PaginaInicial extends Component {
+
+  listarFilmes() {
+    //this.props.listarFilmes();
+    this.props.testeAPI({ email: 'geverson@gmail.com', senha: 'zxc123asd' }, (err) => {
+
+    });
+  }
+
+  componentDidMount() {
+    this.listarFilmes();
+  }
+
+  render() {
+    return (
+      <>
+        <MainBanner />
+        <CarrouselGenre />
+      </>
+    );
+  }
+
 };
 
-export default PaginaInicial;
+export default connect(null, actionsFilmes)(PaginaInicial);
