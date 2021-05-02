26ad02327e35ba5003c6706b73f668b81947bb20
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-02 20:54:47 -0300

    Modificar nomes
    Troca os nomes onde estava 'movie' para 'short' (trad.: curta)

--- src/components/MainCarroussel/index.jsx
+++ src/components/MainCarroussel/index.jsx
@@ -6,11 +6,10 @@ import ButtonArrowRight from '../ButtonArrowRight';
 
 const MainCarroussel = () => {
     return (
-
         <section className='main-carroussel'>
             <ButtonArrowLeft />
-            <img className="movie-thumbnail-carroussel"src={shortThumbnail1} alt="Thumbnail do Curta"/>
-            <h2 className='movie-title-carroussel'>Here Is The Plan</h2>
+            <img className="short-thumbnail-carroussel"src={shortThumbnail1} alt="Thumbnail do Curta"/>
+            <h2 className='short-title-carroussel'>Here Is The Plan</h2>
             <ButtonArrowRight />
         </section>
     )
