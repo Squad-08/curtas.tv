a925451848bb8e37ddc50a05f933136795f1d525
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-02 23:31:59 -0300

    Modificar MainCarroussel
    Modifica o posicionamento do Main Carroussel na pagina inicial da aplicaçao

--- src/components/MainCarroussel/index.jsx
+++ src/components/MainCarroussel/index.jsx
@@ -7,10 +7,12 @@ import ButtonArrowRight from '../ButtonArrowRight';
 const MainCarroussel = () => {
     return (
         <section className='main-carroussel'>
+            <ButtonArrowLeft />
             <div className="wrapper-thumbnail">
                 <img className="short-thumbnail-carroussel"src={shortThumbnail1} alt="Thumbnail do Curta"/>
                 <h2 className="short-title-carroussel">Here Is The Plan</h2>
             </div>
+            <ButtonArrowRight />
         </section>
     )
 }
