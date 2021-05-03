898139b35c0c37fc3220bd1b4d3a7fe6b8e9523c
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-03 14:15:16 -0300

    Criar componente ShortThumbnailCarroussel
    Cria componente responsavel pela thumbnail/banner do curta que aparece no MainCarroussel

---
+++ src/components/MainCarroussel/ShortThumbnailCarroussel/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+import shortThumbnail1 from '../img/shortThumbnail1.png'
+
+const ShortThumbnailCarroussel = () => {
+    return (
+        <img className="short-thumbnail-carroussel"src={shortThumbnail1} alt="Thumbnail do Curta"/>
+    )
+}
+
+export default ShortThumbnailCarroussel
--- src/components/MainCarroussel/index.jsx
+++ src/components/MainCarroussel/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './styles.css';
-import shortThumbnail1 from './img/shortThumbnail1.png'
 import ButtonArrowLeft from '../ButtonArrowLeft';
 import ButtonArrowRight from '../ButtonArrowRight';
 import ShortTitleCarroussel from './ShortTitleCarroussel';
+import ShortThumbnailCarroussel from './ShortThumbnailCarroussel';
 
 const MainCarroussel = () => {
     return (
         <section className='main-carroussel'>
             <ButtonArrowLeft />
             <div className="wrapper-thumbnail">
-                <img className="short-thumbnail-carroussel"src={shortThumbnail1} alt="Thumbnail do Curta"/>
+                <ShortThumbnailCarroussel />
                 <ShortTitleCarroussel />
             </div>
             <ButtonArrowRight />
