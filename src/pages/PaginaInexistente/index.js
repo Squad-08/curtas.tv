be4fdad8f922376c459b37dc54d628c260069776
Geverson <geversonjosedesouza@gmail.com>
2021-05-02 23:19:36 -0300

    Criação de rota e página para rota inexistente

---
+++ src/pages/PaginaInexistente/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const PaginaInexistente = () => {
+    return (
+        <>
+            <div className="area-principal">
+                <div className="area-destaque">
+                    <h1 className="texto">
+                        Página Inexistente
+                    </h1>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default PaginaInexistente;
