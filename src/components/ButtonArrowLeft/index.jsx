ba1ec16de40cbad9db5995bcbe556be2d03a5107
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-05 12:32:58 -0300

    Reafatorar button-arrows do carrossel de generos
    Modifica o button arrow para que nao seja necessario utilizar o pseudo-element para gerar um button em forma de seta

--- src/components/ButtonArrowLeft/index.jsx
+++ src/components/ButtonArrowLeft/index.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 const ButtonArrowLeft = () => {
     return (
-        <button className="button-arrow-left"><span className="direction">Anterior</span></button>
+        <button className="button-arrow-left">▶</button>
     );
 }
 
--- src/components/ButtonArrowRight/index.jsx
+++ src/components/ButtonArrowRight/index.jsx
@@ -3,7 +3,7 @@ import './styles.css'
 
 const ButtonArrowRight = () => {
     return (
-        <button className="button-arrow-right"><span className="direction">Próximo</span></button>
+        <button className="button-arrow-right">▶</button>
     );
 }
 
