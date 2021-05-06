93302a7a784e770c2749a634281e567ff4d3b23d
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 19:25:24 -0300

    Adicionar poster
    Adiciona e estiliza o poster do curta aa segunda tela

--- src/components/SegundaTelaNovo/index.jsx
+++ src/components/SegundaTelaNovo/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './styles.css';
 
 const SegundaTela = () => {
-    return ('');
+    return (
+        <section className="short-data">
+            <img src="https://m.media-amazon.com/images/M/MV5BZWYwZDA5MTEtNzUyOS00ZTNmLWE3YWItZDIwZWQwYzAwNWRiXkEyXkFqcGdeQXVyNzE2Njc3MzM@._V1_.jpg" alt="Poster do Curta" className="short-poster" />
+        </section>
+    );
 }
 
 export default SegundaTela
