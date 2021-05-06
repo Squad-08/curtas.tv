cd402bf502b23e3cde194d928692349b6e13ebc5
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 15:44:47 -0300

    Adicionar titulo a pagina de erro
    Adiciona e estiliza o titulo da pagina de erro

---
+++ src/components/ContentPageNotFound/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const ContentPageNotFound = () => {
+    return (
+        <section className='message-page-not-found'>
+            <h1 className='message-title'>
+                Uhm... O que você está procurando?
+            </h1>
+        </section>
+    );
+}
+
+export default ContentPageNotFound
