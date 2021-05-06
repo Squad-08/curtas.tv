b0c331ac5be4e9f9c7d85b05a5e5ffb0416fef10
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 16:11:52 -0300

    Posicionar titulo e mensagem
    Posiciona como o titulo e a mensagem devem se posicionar um em relaçao ao outro

--- src/components/ContentPageNotFound/index.jsx
+++ src/components/ContentPageNotFound/index.jsx
@@ -4,14 +4,16 @@ import './styles.css';
 const ContentPageNotFound = () => {
     return (
         <section className='message-page-not-found'>
-            <h1 className='message-title'>
-                Uhm... O que você está procurando?
-            </h1>
-            <p className="message">
-                Infelizmente, o que você estava procurando não estava por aqui.
-                Volte a nossa home para encontrar um conteúdo que possa-lhe interessar. 
-            </p>
             <img src="https://images.vexels.com/media/users/3/191488/isolated/preview/ca0eb6479874cbb254f85a11274eb893-a-ccedil-atilde-o-de-c-acirc-mera-de-cinema-by-vexels.png" alt="Camêra Cinematográfica Desenhada" className="message-image" />
+            <div className="wrapper-message">
+                <h1 className='message-title'>
+                    Uhm... O que você está procurando?
+                </h1>
+                <p className="message">
+                    Infelizmente, o que você estava procurando não estava por aqui.
+                    Volte a nossa home para encontrar um conteúdo que possa-lhe interessar. 
+                </p>
+            </div>
         </section>
     );
 }
