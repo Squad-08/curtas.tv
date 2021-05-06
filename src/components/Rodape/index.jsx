54b0ba12baae170ed655befc3548f39fc153b1db
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 12:33:55 -0300

    Adicionar FeitoPor
    Adiciona um segmento ao footer com o nome dos participantes do projeto

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -7,6 +7,13 @@ const Rodape = () => {
             <a href="https://www.fcamara.com.br/" className="site-fcamara">
                 <img className='logo-fcamara' src='https://blog.fcamara.com.br/wp-content/uploads/2019/10/Logotipo-FCamara.png' alt="Logo da FCâmara"></img>
             </a>
+            <h3 className="made-by">
+                Feito Por:
+                Geverson Souza
+                Giselle Piasetzki
+                Matheus Leonardo
+                WagnerCarestini
+            </h3>
         </footer>
     );
 }
