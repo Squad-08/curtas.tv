ab2ade8fa4fa11269ccfc07d5253b51ddbfd8747
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 19:32:25 -0300

    Adicionar titulo do curta
    Adiciona o titulo do curta a segunda tela e estiliza-o

--- src/components/SegundaTelaNovo/index.jsx
+++ src/components/SegundaTelaNovo/index.jsx
@@ -5,6 +5,7 @@ const SegundaTela = () => {
     return (
         <section className="short-data">
             <img src="https://m.media-amazon.com/images/M/MV5BZWYwZDA5MTEtNzUyOS00ZTNmLWE3YWItZDIwZWQwYzAwNWRiXkEyXkFqcGdeQXVyNzE2Njc3MzM@._V1_.jpg" alt="Poster do Curta" className="short-poster" />
+            <h1 className="short-title">Título do Curta</h1>
         </section>
     );
 }
