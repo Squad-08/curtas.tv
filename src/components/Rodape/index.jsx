e6cc27668aa667d0352805f4085e1a42eab2e375
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 12:27:54 -0300

    Adicionar logo FCamara
    Adiciona o logo da FCamara e modifica-o para que ele tenha as dimensoes adequadas

--- src/components/Rodape/index.jsx
+++ src/components/Rodape/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles.css'
-import LogoFCamara from './img/logo-fcamara.png';
 
 const Rodape = () => {
     return (
         <footer className="main-footer">
+            <a href="https://www.fcamara.com.br/" className="site-fcamara">
+                <img className='logo-fcamara' src='https://blog.fcamara.com.br/wp-content/uploads/2019/10/Logotipo-FCamara.png' alt="Logo da FCâmara"></img>
+            </a>
         </footer>
     );
 }
