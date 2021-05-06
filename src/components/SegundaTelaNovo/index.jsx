642d42bdc38061a6569c3a52f5e09937a0d90b60
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 19:46:38 -0300

    Posicionar titulo e sinopse
    Posiciona o titulo e a sinopse um em relaçao ao outro

--- src/components/SegundaTelaNovo/index.jsx
+++ src/components/SegundaTelaNovo/index.jsx
@@ -5,14 +5,16 @@ const SegundaTela = () => {
     return (
         <section className="short-data">
             <img src="https://m.media-amazon.com/images/M/MV5BZWYwZDA5MTEtNzUyOS00ZTNmLWE3YWItZDIwZWQwYzAwNWRiXkEyXkFqcGdeQXVyNzE2Njc3MzM@._V1_.jpg" alt="Poster do Curta" className="short-poster" />
-            <h1 className="short-title">Título do Curta</h1>
-            <p className="short-sinopse">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta praesentium rem eius cum.
-                Quidem hic explicabo at, tempora consectetur,
-                quisquam consequuntur quaerat odio
-                similique alias iusto modi
-                quae voluptatibus molestiae.
-            </p>
+            <div className="wrapper-data">
+                <h1 className="short-title">Título do Curta</h1>
+                <p className="short-sinopse">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta praesentium rem eius cum.
+                    Quidem hic explicabo at, tempora consectetur,
+                    quisquam consequuntur quaerat odio
+                    similique alias iusto modi
+                    quae voluptatibus molestiae.
+                </p>
+            </div>
         </section>
     );
 }
