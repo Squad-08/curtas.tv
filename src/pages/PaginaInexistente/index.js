c932173c98f9f551555bed0e526f0598e9798773
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-06 15:33:22 -0300

    Preparar PageNotFound
    Prepara a pagina para que ela possa receber os arquivos necessarios para sua modificaçao

--- src/pages/PaginaInexistente/index.js
+++ src/pages/PaginaInexistente/index.js
@@ -1,18 +1,8 @@
-import React from 'react';
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
 const PaginaInexistente = () => {
-    return (
-        <>
-            <div className="area-principal">
-                <div className="area-destaque">
-                    <h1 className="texto">
-                        Página Inexistente
-                    </h1>
-                </div>
-            </div>
-        </>
-    );
-}
+  return "";
+};
 
 export default PaginaInexistente;
