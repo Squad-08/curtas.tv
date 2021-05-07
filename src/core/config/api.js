d23bbfc4ee63753db31585d53bb8e9fb2f68ac39
Geverson <geversonjosedesouza@gmail.com>
2021-05-06 22:46:02 -0300

    Ajustando a URL da API para definitiva

--- src/core/config/api.js
+++ src/core/config/api.js
@@ -1,1 +1,1 @@
-export const api = (process.env.PUBLIC_URL) ? "URL_PUBLICA_DA_API" : "http://localhost:8081";
+export const api = (process.env.PUBLIC_URL) ? "URL_PUBLICA_DA_API" : "http://localhost:3333";
