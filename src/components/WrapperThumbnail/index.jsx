5dafe29244f9ec60412b557984a6e58fb57f0453
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-08 18:55:07 -0300

    Redirecionar para pagina
    Redireciona o usuario para a pagina do curta ao clicar em um dos curtas presentes no destaque

--- src/components/WrapperThumbnail/index.jsx
+++ src/components/WrapperThumbnail/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './styles.css';
 import ShortThumbnailCarroussel from '../ShortThumbnailCarroussel';
 import ShortTitleCarroussel from '../ShortTitleCarroussel';
+import { Link } from 'react-router-dom';
 
 const WrapperThumbnail = (props) => {
     return (
-        <div className="wrapper-thumbnail">
-            <ShortThumbnailCarroussel src={props.src} alt={props.alt} key={props.key} />
-            <ShortTitleCarroussel titleCarrossel={props.titleCarrossel} key={props.key}/>
-        </div>
+        <Link to={`/filme/${props.id}`}>
+            <div className="wrapper-thumbnail">
+                <ShortThumbnailCarroussel src={props.src} alt={props.alt} key={props.key} />
+                <ShortTitleCarroussel titleCarrossel={props.titleCarrossel} key={props.key}/>
+            </div>
+        </Link>
     )
 }
 
