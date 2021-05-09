0eeb261d57ed9dfbcc25fa334b68f3433063b14f
Matheus Leonardo <matheusleotrab@gmail.com>
2021-05-09 20:50:52 -0300

    Corrigir com o Lucas Melo
    Correçao do problema de centralizaçao de titulo do ShortThumbnailTitle

--- src/components/ShortTitleCarroussel/index.jsx
+++ src/components/ShortTitleCarroussel/index.jsx
@@ -3,7 +3,9 @@ import './styles.css';
 
 const ShortTitleCarroussel = (props) => {
     return (
-        <h2 className="short-title-carroussel" key={props.key}>{props.titleCarrossel}</h2>
+        <div className="wrapper-short-title-carroussel">
+            <h2 className="short-title-carroussel" key={props.key}>{props.titleCarrossel}</h2>
+        </div>
     )
 }
 
